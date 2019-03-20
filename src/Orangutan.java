51b774444edc0b8b14b3455e848a9e911679b6fa
razornev <razornev@gmail.com>
2019-03-20 18:33:14 +0100

    Controller implementálva, Armchair implementálva, Moveable implementálva, Orangután implementálva Kommentek hiányoznak, kommentelem majd

--- src/Orangutan.java
+++ src/Orangutan.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author ricsi
- */
+
 public class Orangutan extends Moveable{
  
-    private Controller controller;
+    private static final Controller CONTROLLER = Controller.getInstance();
     private int points = 0;
 
 	/**
@@ -18,18 +10,20 @@ public class Orangutan extends Moveable{
 	 * @param p
 	 */
 	public void addPoints(int p) {
-		// TODO - implement Orangutan.addPoints
-		throw new UnsupportedOperationException();
+		points += p;
 	}
 
 	public void leadOut() {
-		// TODO - implement Orangutan.leadOut
-		throw new UnsupportedOperationException();
+            
+		if(holdsPanda != null){
+                    addPoints(holdsPanda.count(0));
+                    holdsPanda.ledOut();
+                }
 	}
-
+        
+        @Override
 	public void fall() {
-		// TODO - implement Orangutan.fall
-		throw new UnsupportedOperationException();
+            CONTROLLER.endGame();
 	}
 
 	/**
@@ -37,17 +31,17 @@ public class Orangutan extends Moveable{
 	 * @param pnd
 	 */
 	public void add(Panda pnd) {
-		// TODO - implement Orangutan.add
-		throw new UnsupportedOperationException();
+		if(holdsPanda != null){
+                    holdsPanda.setHeldByMoveable(pnd);
+                    pnd.setHoldsPanda(holdsPanda);
+                }
+                holdsPanda = pnd;
+                holdsPanda.setHeldByMoveable(this);
 	}
 
     @Override
-    public void hitBy(Orangutan o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void hitBy(Orangutan o) {}
 
     @Override
-    public void hitBy(Panda p) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void hitBy(Panda p) {}
 }
