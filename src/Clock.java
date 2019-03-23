915791a21eba5ccf1874e4cc0380ba7fdf96ec89
franciskav <franciska.vigh@gmail.com>
2019-03-23 01:21:55 +0100

    Clock kész

--- src/Clock.java
+++ src/Clock.java
@@ -1,21 +1,13 @@
 
 import java.util.List;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ricsi
- */
 public class Clock {
     
     private List<Steppable> steppable;
-    	public void tick() {
-		// TODO - implement Clock.tick
-		throw new UnsupportedOperationException();
-	}
+    
+    public void tick() {
+        steppable.forEach((st) -> {
+            st.step();
+    });
+    }
 }
